121470aac87f536432c050f5a874c3b22d696f08
dev_apus <folorunsopraise580@gmail.com>
2024-10-04 21:22:23 +0100

    finalize

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
       "http://localhost:3500",
-      "http://localhost:5173"
+      "http://localhost:5173",
+      process.env.USER_FRONTEND
 ];
 
 module.exports=allowedOrigins
