705a75d1d96507e6639cd0ebfb59e2b4f77fdc7a
Folorunso praise Oluwapelumi <106927972+praisezee@users.noreply.github.com>
2024-10-07 13:30:20 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
       "http://localhost:3500",
       "http://localhost:5173",
-      process.env.USER_FRONTEND
+      "https://bayprof.com",
+      "https://api.bayprof.com",
+      `${process.env.USER_FRONTEND}`
 ];
 
 module.exports=allowedOrigins
